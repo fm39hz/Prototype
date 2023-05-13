c5ea71273ca44bd46185dd6a5ed25190f1b97afe
fm39hz <hitpoint2k3@gmail.com>
2023-05-13 19:53:15 +0700

    Bổ sung document

--- Script/System/Object/DynamicObject.cs
+++ Script/System/Object/DynamicObject.cs
@@ -5,11 +5,23 @@ using Component.Animation;
 using Component.StateMachine;
 
 namespace Game.Object{
+	/// <summary>
+	/// Object động, có State Machine, có Animation
+	/// </summary>
     public abstract partial class DynamicObject : CharacterBody2D{
+		/// <summary>
+		/// Lưu giá trị kiểm tra xem có đang collide với vật thể nào không
+		/// </summary>
+		/// <value></value>
         public bool IsCollided{get; protected set;} = false;
 		public SpriteSheet Sheet{get; protected set;}
-		public StateMachine ObjectedStateMachine{get; protected set;}
+		public StateMachine ObjectiveStateMachine{get; protected set;}
 		public DynamicMetadata Metadata{get; protected set;}
+		/// <summary>
+		/// Trả về node con đầu tiên có type T
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <returns>Node con đầu tiên có type T, hoặc null nếu không tìm thấy</returns>
 		public T GetFirstChildOfType<T>() where T : Node{
 			T targetChild = null;
 				for (int i = 0; i < this.GetChildCount(); i++){
@@ -20,6 +32,11 @@ namespace Game.Object{
 					}
 			return targetChild;
 			}
+		/// <summary>
+		/// Trả về node đầu tiên có type T trong node Cha, ở cùng cấp với node hiện tại
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <returns>Node đầu tiên có type T ở cùng cấp, hoặc null nếu không tìm thấy</returns>
 		public T GetFirstSiblingOfType<T>() where T : Node{
 			Node parent = this.GetParent();
 			T targetSibling = null;
@@ -36,34 +53,60 @@ namespace Game.Object{
 				this.Sheet = GetFirstChildOfType<SpriteSheet>();
 				}
 			catch (InvalidCastException e){
-				GD.Print(e.Message);
+				GD.Print("Không thể cast tới Sprite Sheet");
+				throw e;
 				}
 			catch (NullReferenceException e){
-				GD.Print(e.Message);
+				GD.Print("Chưa có Sprite Sheet");
+				throw e;
 				}
 			}
 		public override void _Ready(){
 			try{
-				this.ObjectedStateMachine = GetFirstChildOfType<StateMachine>();
+				this.ObjectiveStateMachine = GetFirstChildOfType<StateMachine>();
 				this.Metadata = new();
 				}
 			catch (InvalidCastException e){
-				GD.Print(e.Message);
+				GD.Print("Không thể cast tới State Machine");
+				throw e;
 				}
 			catch (NullReferenceException e){
-				GD.Print(e.Message);
+				GD.Print("Chưa có State Machine");
+				throw e;
 				}
 			}
+		public override void _PhysicsProcess(double delta){
+			UpdateMetadata();
+			Animation(delta);
+			this.IsCollided = MoveAndSlide();
+			}
+		/// <summary>
+		/// Trả về giá trị bằng fps * delta
+		/// </summary>
+		/// <param name="delta"></param>
+		/// <returns>1 khi fps đạt ngưỡng lý tưởng</returns>
 		protected static double GetRelativeResponseTime(double delta){
 			return Performance.GetMonitor(Performance.Monitor.TimeFps) * delta;
 			}
-		protected void UpdateMetaData(){
-			var currentState = ObjectedStateMachine.CurrentState.ToDynamic();
-				Metadata.StateID = currentState.ID;
-				Metadata.IsLoopingAnimation = currentState.IsLoop;
-					if (!Velocity.IsEqualApprox(Vector2.Zero)){
-						Metadata.SetDirection(Velocity);
+		/// <summary>
+		/// Cập nhật Metadata của đối tượng
+		/// </summary>
+		protected void UpdateMetadata(){
+			var State = this.ObjectiveStateMachine.CurrentState.ToDynamic();
+				this.Metadata.StateID = State.ID;
+				this.Metadata.IsLoopingAnimation = State.IsLoop;
+					if (!this.Velocity.IsEqualApprox(Vector2.Zero)){
+						this.Metadata.SetDirection(Velocity);
 						}
+			}
+		/// <summary>
+		/// Animate Sprite Sheet dựa trên thông tin lấy được từ method UpdateMetadata
+		/// </summary>
+		/// <param name="delta"></param>
+		protected void Animation(double delta){
+			var State = this.ObjectiveStateMachine.CurrentState.ToDynamic();
+			var Frame = State.Frame;
+				this.Sheet.Animate(Frame, Metadata, GetRelativeResponseTime(delta));
 			}
         }
     }
