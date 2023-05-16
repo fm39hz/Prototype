4e4bdb95aef98a90a0a75b5006de5d67e738137b
fm39hz <hitpoint2k3@gmail.com>
2023-05-16 14:35:30 +0700

    remove unecessary code

--- Script/Attach/Player/Player.cs
+++ Script/Attach/Player/Player.cs
@@ -1,5 +1,3 @@
-using Godot;
-using System;
 using Management.InputManager;
 
 namespace Game.Object.Dynamic{
@@ -7,16 +5,8 @@ namespace Game.Object.Dynamic{
 		public PlayerInputManager InputManager{get; private set;}
 		public override void _EnterTree(){
 			base._EnterTree();
-				try{
-					var _scene = GetParent();
-					InputManager = GetFirstChildOfType<PlayerInputManager>();
-					}
-				catch (InvalidCastException e){
-					GD.Print(e.Message);
-					}
-				catch (NullReferenceException e){
-					GD.Print(e.Message);
-					}
+				var _scene = GetParent();
+				InputManager = GetFirstChildOfType<PlayerInputManager>();
 			}
 		}
 	}
