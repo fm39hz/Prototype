7328243877749c0df24c378ab04013d7f1210299
fm39hz <hitpoint2k3@gmail.com>
2023-05-24 18:52:41 +0700

    update document

--- Script/System/Metadata/FrameData.cs
+++ Script/System/Metadata/FrameData.cs
@@ -1,10 +1,21 @@
 namespace Metadata.Animation{
+    /// <summary>
+    /// Chứa thông tin về frame của 1 đối tượng
+    /// </summary>
     public class FrameData{
         public FrameData(int frameNumber, double speed){
             Length = frameNumber;
             Speed = speed;
             }
+        /// <summary>
+        /// Số frame của Animation
+        /// </summary>
+        /// <value></value>
         public int Length{get; set;}
+        /// <summary>
+        /// Tốc độ của Animation
+        /// </summary>
+        /// <value></value>
         public double Speed{get; set;}
         }
     }
