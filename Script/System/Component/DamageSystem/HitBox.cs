6f05ff50a0bf9bf8f773448f863f349333f76c39
fm39hz <hitpoint2k3@gmail.com>
2023-06-01 18:12:59 +0700

    remove Hitbox draft

--- Script/System/Component/DamageSystem/HitBox.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-using Godot;
-
-namespace Component.DamageSystem;
-    public partial class Hitbox : Area2D{
-        [Signal] public delegate void AttackHittedEventHandler();
-        }
