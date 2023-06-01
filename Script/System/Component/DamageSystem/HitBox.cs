03ebc3e8682ef7725a4f97022674599f0360de53
fm39hz <hitpoint2k3@gmail.com>
2023-06-01 17:26:39 +0700

    remove illogical param

--- Script/System/Component/DamageSystem/HitBox.cs
+++ Script/System/Component/DamageSystem/HitBox.cs
@@ -1,10 +1,7 @@
+using System.Collections.Generic;
 using Godot;
 
 namespace Component.DamageSystem;
-    public partial class Hitbox : Node{
-        [Signal] delegate void AttackHittedEventHandler();
-        [Export] public Area2D AttackZone{get; set;}
-        public virtual void DoAttack(){
-
-            }
+    public partial class Hitbox : Area2D{
+        [Signal] public delegate void AttackHittedEventHandler();
         }
