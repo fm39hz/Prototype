6c1f472ad87778f182b3f6797eb2bc139c20cce7
fm39hz <hitpoint2k3@gmail.com>
2023-06-14 23:54:44 +0700

    fixed naming convention

--- Script/System/Component/Animation/SpriteSheet.cs
+++ Script/System/Component/Animation/SpriteSheet.cs
@@ -14,11 +14,11 @@ namespace Component.Animation{
         /// <summary>
         /// Frame hiện tại
 		/// </summary>
-        private int currentFrame{get; set;} = 0;
+        private int CurrentFrame{get; set;} = 0;
         /// <summary>
         /// Bộ đếm frame thực
 		/// </summary>
-        private double frameCounter{get; set;} = 0;
+        private double FrameCounter{get; set;} = 0;
         /// <summary>
         /// Chạy animation của Sprite Sheet Dữ liệu của đối tượng được truyền vào
         /// </summary>
@@ -29,25 +29,25 @@ namespace Component.Animation{
             var _direction = objectData.GetDirectionAsNumber();     //Lấy hướng nhìn của đối tượng
             var _firstFrame = frameInfo.Length * _direction++;      //Lấy frame bắt đầu của animation
             var _nextFrame = frameInfo.Length * _direction;         //Lấy frame bắt đầu của hướng kế tiếp
-                if (_firstFrame <= currentFrame && currentFrame < _nextFrame){
-                    frameCounter += relativeResponseTime;       //Tạo bộ đếm frame(thực)
+                if (_firstFrame <= CurrentFrame && CurrentFrame < _nextFrame){
+                    FrameCounter += relativeResponseTime;       //Tạo bộ đếm frame(thực)
                     }
-                if (frameCounter >= 60 * relativeResponseTime / frameInfo.Speed){
-                    if (currentFrame == _nextFrame - 1){
+                if (FrameCounter >= 60 * relativeResponseTime / frameInfo.Speed){
+                    if (CurrentFrame == _nextFrame - 1){
                         if (!objectData.IsLoopingAnimation){
                             EmitSignal(SignalName.AnimationFinished);
                             }
-                        currentFrame = _firstFrame;             //Reset về frame bắt đầu khi tới frame cuối
+                        CurrentFrame = _firstFrame;             //Reset về frame bắt đầu khi tới frame cuối
                         }
-                    else if (currentFrame < _nextFrame){
-                        currentFrame++;                         //Tăng frame lên 1 khi chưa chạm tới frame cuối
+                    else if (CurrentFrame < _nextFrame){
+                        CurrentFrame++;                         //Tăng frame lên 1 khi chưa chạm tới frame cuối
                         }
-                    frameCounter = 0;                           //Reset bộ đếm frame(thực)
+                    FrameCounter = 0;                           //Reset bộ đếm frame(thực)
                     }
-                if (currentFrame < _firstFrame || currentFrame > _nextFrame){
-                    currentFrame = _firstFrame;                 //Chuyển tiếp frame tới vị trí mới
+                if (CurrentFrame < _firstFrame || CurrentFrame > _nextFrame){
+                    CurrentFrame = _firstFrame;                 //Chuyển tiếp frame tới vị trí mới
                     }
-            FrameCoords = new Vector2I(currentFrame, objectData.StateID);
+            FrameCoords = new Vector2I(CurrentFrame, objectData.StateID);
             }
         }
     }
