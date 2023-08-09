f6af207a69c8910daeb53647b9b218ea9e244812
fm39hz <hitpoint2k3@gmail.com>
2023-08-09 17:59:20 +0700

    Improve some Debug data

--- Script/Actor/Player/State/Walk.cs
+++ Script/Actor/Player/State/Walk.cs
@@ -1,20 +1,28 @@
 using GameSystem.Component.FiniteStateMachine;
 
-namespace  Actor.Player;
-	public partial class Walk : DynamicState{
-		public override void _Ready(){
-			base._Ready();
-			Object.InputManager.MovementKeyPressed += SetCondition;
-			Object.InputManager.ActionKeyPressed += ResetCondition;
-			}
-		// public override void SetCondition(bool condition){
-		// 	base.SetCondition(condition);
-		// 	if (Object.Velocity.IsZeroApprox() && !StateController.PreviousState.IsState(this)){
-		// 		Condition = false;
-		// 		}
-		// 	}
-		public override void RunningState(double delta){
-			base.RunningState(delta);
-			Object.Velocity = Object.InputManager.TopDownVector(Object.Velocity) * MovingSpeed;
-			}
-		}
+namespace Actor.Player;
+
+public partial class Walk : DynamicState
+{
+	public override void _Ready()
+	{
+		base._Ready();
+		Object.InputManager.MovementKeyPressed += SetCondition;
+		Object.InputManager.ActionKeyPressed += ResetCondition;
+	}
+
+	// public override void SetCondition(bool condition)
+	// {
+	// 	base.SetCondition(condition);
+	// 	if (Object.Velocity.IsZeroApprox() && !StateController.PreviousState.IsState(this))
+	// 	{
+	// 		Condition = false;
+	// 	}
+	// }
+
+	public override void RunningState(double delta)
+	{
+		base.RunningState(delta);
+		Object.Velocity = Object.InputManager.TopDownVector(Object.Velocity) * MaxSpeed;
+	}
+}
