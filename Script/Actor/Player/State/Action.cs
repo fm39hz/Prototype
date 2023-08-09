93178d772adfbdceb398f9985e74491bddfc357a
fm39hz <hitpoint2k3@gmail.com>
2023-08-09 18:01:22 +0700

    Reformat code

--- Script/Actor/Player/State/Action.cs
+++ Script/Actor/Player/State/Action.cs
@@ -1,21 +1,30 @@
 using GameSystem.Component.FiniteStateMachine;
 
-namespace  Actor.Player;
-	public partial class Action : DynamicState{
-		public override void _Ready(){
-			base._Ready();
-			Object.InputManager.ActionKeyPressed += SetCondition;
-			Object.Sheet.AnimationFinished += ResetCondition;
-			}
-		public void SetCondition(){
-			base.SetCondition(true);
-			}
-		public override void ResetCondition(){
-			base.ResetCondition();
-			Object.IsMoveable = true;
-			}
-		public override void RunningState(double delta){
-			base.RunningState(delta);
-			Object.Velocity = Object.InputManager.TopDownVector(Object.Metadata.GetDirectionAsVector()) * MovingSpeed;
-			}
-		}
+namespace Actor.Player;
+
+public partial class Action : DynamicState
+{
+	public override void _Ready()
+	{
+		base._Ready();
+		Object.InputManager.ActionKeyPressed += SetCondition;
+		Object.Sheet.AnimationFinished += ResetCondition;
+	}
+
+	public void SetCondition()
+	{
+		base.SetCondition(true);
+	}
+
+	public override void ResetCondition()
+	{
+		base.ResetCondition();
+		Object.IsMoveable = true;
+	}
+
+	public override void RunningState(double delta)
+	{
+		base.RunningState(delta);
+		Object.Velocity = Object.InputManager.TopDownVector(Object.Information.GetDirectionAsVector()) * MaxSpeed;
+	}
+}
--- Script/Actor/Player/State/Idle.cs
+++ Script/Actor/Player/State/Idle.cs
@@ -1,17 +1,24 @@
 using GameSystem.Component.FiniteStateMachine;
 
-namespace  Actor.Player;
-	public partial class Idle : DynamicState{
-		public override void _Ready(){
-			base._Ready();
-			var _inputManager = Object.InputManager;
-			_inputManager.MovementKeyPressed += SetCondition;
-			_inputManager.ActionKeyPressed += ResetCondition;
-			}
-		public override void SetCondition(bool condition){
-			if (!IsInitialized){
-				return;
-				}
-			Condition = !condition;
-			}
+namespace Actor.Player;
+
+public partial class Idle : DynamicState
+{
+	public override void _Ready()
+	{
+		base._Ready();
+		var _inputManager = Object.InputManager;
+		_inputManager.MovementKeyPressed += SetCondition;
+		_inputManager.ActionKeyPressed += ResetCondition;
+	}
+
+	public override void SetCondition(bool condition)
+	{
+		if (!IsInitialized)
+		{
+			return;
 		}
+
+		Condition = !condition;
+	}
+}
