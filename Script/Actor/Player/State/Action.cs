c16511709acdcd1a4d76c987fa9ea81a4d6ee525
fm39hz <hitpoint2k3@gmail.com>
2023-09-17 21:53:40 +0700

    update Action with new System

--- Script/Actor/Player/State/Action.cs
+++ Script/Actor/Player/State/Action.cs
@@ -1,27 +1,28 @@
+using System.IO;
 using GameSystem.Component.FiniteStateMachine;
+using GameSystem.Component.Object.Composition;
 using GameSystem.Component.Object.Compositor;
 using GameSystem.Data.Instance;
+using GameSystem.Object.Compositor.Implemented;
 using GameSystem.Utils;
 
 namespace Attach.PlayerState;
 
-using Actor;
-
 public partial class Action : StaticState
 {
-	public PlayerBody Target { get; private set; }
+	public Player Target { get; private set; }
 
 	public override void _EnterTree()
 	{
 		base._EnterTree();
-		Target = StateMachine.GetOwner<CreatureCompositor>().GetFirstChild<PlayerBody>();
+		Target = GetOwner<Player>();
 	}
 
 	public override void _Ready()
 	{
 		base._Ready();
-		Target.InputManager.ActionKeyPressed += SetCondition;
-		Target.Compositor.SpriteSheet.AnimationFinished += ResetCondition;
+		Target.InputHandler.ActionKeyPressed += SetCondition;
+		Target.SpriteSheet.AnimationFinished += ResetCondition;
 	}
 
 	public void SetCondition()
@@ -32,7 +33,7 @@ public partial class Action : StaticState
 	public override void ResetCondition()
 	{
 		base.ResetCondition();
-		if (Target.Compositor.Information is CreatureData _information)
+		if (Target.Information is CreatureData _information)
 		{
 			_information.IsMoveable = true;
 		}
@@ -41,7 +42,11 @@ public partial class Action : StaticState
 	public override void RunningState(double delta)
 	{
 		base.RunningState(delta);
-		Target.Velocity = Target.InputManager.TopDownVector(Target.Compositor.Information.Direction.AsVector) *
+		if (Target.Composition is not Creature _target)
+		{
+			throw new InvalidDataException("Player Must be Creature");
+		}
+		_target.Velocity = Target.InputHandler.TopDownVector(Target.Information.Direction.AsVector) *
 						MaxSpeed;
 	}
 }
